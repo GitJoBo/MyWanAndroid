abad2dd44085f2075ce562013bb23cffb4f79412
admin <wangshijun@ji-du.com.cn>
2021-08-18 17:30:42 +0800

    VersionPlugin统一版本管理

---
+++ app/src/main/java/com/jobo/mywanandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jobo.mywanandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
